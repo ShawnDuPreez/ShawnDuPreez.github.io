dc576b18090083865a92e38bcdd74babff206c7b
Shawn du Preez <Shawn345dp@gmail.com>
2025-11-13 13:13:22 +0200

    Clean up: Remove unused files (Vite config, Next.js project, old src files, duplicates)

--- start_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\v6dri\OneDrive\Documents\Portfolio"
-echo Starting server in Portfolio directory...
-echo Server will run at http://localhost:8000
-echo Press Ctrl+C to stop the server
-python -m http.server 8000
-
